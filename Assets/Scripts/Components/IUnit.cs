1ceb5f78e1eb78594e08ba5493900f671474fc61
tristar <tristar.2nd@gmail.com>
2021-01-10 22:42:36 +0900

    補足のコメントを追加

--- Assets/Scripts/Components/IUnit.cs
+++ Assets/Scripts/Components/IUnit.cs
@@ -5,19 +5,46 @@ namespace ActionSample.Components
 {
     public interface IUnit : IComponent
     {
+        /// <summary>
+        /// 指定方向に歩かせる
+        /// </summary>
+        /// <param name="x">X方向の移動量</param>
+        /// <param name="z">Z方向の移動量</param>
         void MoveToward(float x, float z);
 
+        /// <summary>
+        ///指定した方向に力を加える
+        /// </summary>
+        /// <param name="force">加速度</param>
         void AddForce(Vector3 force);
+
+        /// <summary>
+        /// 加速度をゼロにして停止させる
+        /// </summary>
         void StopForce();
 
+        /// <summary>
+        /// 現在の状態を返す
+        /// </summary>
         States GetState();
+
+        /// <summary>
+        /// 現在の状態を強制的にセットする
+        /// </summary>
+        /// <param name="newState"></param>
         void SetState(States newState);
 
+        /// <summary>
+        /// 向いている方向
+        /// </summary>
         Dimension dimension
         {
             get; set;
         }
 
+        /// <summary>
+        /// 現在の加速度
+        /// </summary>
         Vector3 velocity
         {
             get; set;
